86461ff1f1a3dac448d58dc8697b25241614a3c6
Daniel Harris <danielharris@gatech.edu>
2022-09-30 02:15:41 +0300

    File reorganization & character attribute system

---
+++ Assets/Scripts/Character/Modifiers/HealthModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthModifier : CharacterModifier
+{
+	public float HealthAmount { get; set; }
+	
+	public HealthModifier(float healthAmount, float duration = 1)
+	{
+		EffectedAttributes = new []{"Health"};
+		Duration = duration;
+		HealthAmount = healthAmount;
+	}
+	
+	public override void OnAddModifier(CharacterAttributeItems attributes)
+	{
+		attributes.Health += HealthAmount;
+	}
+}
