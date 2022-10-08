f7ea012ac43d1753cee97f814e502db1e930e509
Paul Bockelmann <paul@MacBook-Pro-von-Paul.fritz.box>
2022-10-08 23:15:09 +0200

    added bullet trail

--- Assets/Scripts/Weapons/MachineGunFire.cs
+++ Assets/Scripts/Weapons/MachineGunFire.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(Animator))]
 public class MachineGunFire : MonoBehaviour
 {
     public ParticleSystem system;
+
+    [SerializeField]
+    private bool bulletSpread = true;
+    [SerializeField]
+    private Vector3 bulletSpreadVariance = new Vector3(0.05f, 0.05f, 0.05f);
+    [SerializeField]
+    //private ParticleSystem shootingSystem;
+    //[SerializeField]
+    //private ParticleSystem impactParticleSystem;
+    //[SerializeField]
+    private TrailRenderer bulletTrail;
+    [SerializeField]
+    private Transform bulletSpawnPoint;
 
     private void Start()
     {
@@ -26,8 +40,48 @@ public class MachineGunFire : MonoBehaviour
         {
             system.enableEmission = true;
             system.Play();
+
+            Vector3 direction = GetDirection();
+            if (Physics.Raycast(bulletSpawnPoint.position, direction, out RaycastHit hit, float.MaxValue))
+            {
+                TrailRenderer trail = Instantiate(bulletTrail, bulletSpawnPoint.position, Quaternion.identity);
+                StartCoroutine(SpawnTrail(trail, hit));
+            }
             yield return null;
         }
         system.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+    }
+
+    private Vector3 GetDirection()
+    {
+        Vector3 direction = transform.forward;
+        if (bulletSpread)
+        {
+            direction += new Vector3(
+                Random.Range(-bulletSpreadVariance.x, bulletSpreadVariance.x),
+                Random.Range(-bulletSpreadVariance.y, bulletSpreadVariance.y),
+                Random.Range(-bulletSpreadVariance.z, bulletSpreadVariance.z)
+                );
+            direction.Normalize();
+
+        }
+        return direction;
+    }
+
+    private IEnumerator SpawnTrail(TrailRenderer Trail, RaycastHit Hit)
+    {
+        float time = 0;
+        Vector3 startPosition = Trail.transform.position;
+        while (time < 1)
+        {
+            Trail.transform.position = Vector3.Lerp(startPosition, Hit.point, time);
+            time += Time.deltaTime / Trail.time;
+
+            yield return null;
+        }
+        Trail.transform.position = Hit.point;
+        // Instantiate(impactParticleSystem, Hit.point, Quaternion.LookRotation(Hit.normal));
+
+        Destroy(Trail.gameObject, Trail.time);
     }
 }
