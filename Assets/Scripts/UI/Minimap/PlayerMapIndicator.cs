ca5c8ef1bec2d648740fbd9083ade5b61c54a68b
Yeshurun <itai.yeshurun@intel.com>
2022-12-04 23:51:11 +0200

    minimap alignment/color

--- Assets/Scripts/UI/Minimap/PlayerMapIndicator.cs
+++ Assets/Scripts/UI/Minimap/PlayerMapIndicator.cs
@@ -5,15 +5,17 @@ namespace UI.Minimap
 {
     public class PlayerMapIndicator : MonoBehaviour
     {
+        public Color PlayerColor;
+        public Color EnemyColor;
         private Boolean _isAIAgent;
         private SpriteRenderer _spriteRenderer;
-        
+
         private void Awake()
         {
             _isAIAgent = gameObject.CompareTag("AI");
             var indicator = gameObject.transform.Find("MapIndicator");
             _spriteRenderer = indicator.gameObject.GetComponent<SpriteRenderer>();
-            _spriteRenderer.color = _isAIAgent ? Color.red : Color.green;
+            _spriteRenderer.color = _isAIAgent ? Color.red : PlayerColor;
         }
     }
 }
